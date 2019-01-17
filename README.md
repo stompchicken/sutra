42f14f56348fbc8c408263c30d69d10287f11269
Stephen Spencer <steve@stompchicken.com>
2019-01-17 07:37:34 +0000

    Add python packaging and testing

---
+++ README.md
@@ -0,0 +1,4 @@
+Sutra
+=====
+
+[![Build Status](https://travis-ci.com/stompchicken/sutra.svg?branch=master)](https://travis-ci.com/stompchicken/sutra)
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+
+root_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(root_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='sutra',
+    version='0.0.1',
+    description='Natural language processing in PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/stompchicken/sutra',
+    author='Stephen Spencer',
+    author_email='sutra@stompchicken.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['sutra', 'tests']),
+    install_requires=['torch', 'numpy', 'pandas', 'requests']
+)
